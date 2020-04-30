6c824c3053fd70f2f3260a9aeb1d388f08de04fa
Ihwan <mynameisihwan@gmail.com>
2020-04-30 17:18:06 +0700

    Initial Commit

---
+++ wonderfulbatam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wonderfulbatam
+//
+//  Created by Ihwan ID on 30/04/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
